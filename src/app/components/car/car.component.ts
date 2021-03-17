bf1f59f71a3107957e3bf3277fe7db7dafde1104
Atacan Turan <tatacan61@gmail.com>
2021-03-17 19:55:22 +0300

    Clean unnecessary libraries

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Brand } from 'src/app/models/brand';
 import { Car } from 'src/app/models/car';
-import { CarResponseModel } from 'src/app/models/carResponseModel';
-import { Color } from 'src/app/models/color';
-import { BrandService } from 'src/app/services/brand/brand.service';
 import { CarService } from 'src/app/services/car/car.service';
-import { ColorService } from 'src/app/services/color/color.service';
 
 @Component({
   selector: 'app-car',
